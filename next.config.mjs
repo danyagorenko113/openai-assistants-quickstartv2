d74068035c1cb842b9219a0ce148e20e01f20567
danyagorenko113 <dgorenko115@gmail.com>
2024-11-08 14:27:27 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   webpack: (config) => {
     config.externals = [...config.externals, 'canvas', 'jsdom'];
     return config;
