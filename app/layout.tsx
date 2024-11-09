18a6b67989ecc9daa3654e17ca3bc9f3bd892e15
danyagorenko113 <dgorenko115@gmail.com>
2024-11-09 00:00:49 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import { Inter } from "next/font/google";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 import Warnings from "./components/warnings";
 import { assistantId } from "./assistant-config";
-const inter = Inter({ subsets: ["latin"] });
+
+const mulish = Mulish({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Assistants API Quickstart",
@@ -12,11 +13,18 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {assistantId ? children : <Warnings />}
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/openai.svg" />
+      </head>
+      <body className={mulish.className}>
+        <main>
+          {assistantId ? children : <Warnings />}
+        </main>
         <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
       </body>
     </html>
